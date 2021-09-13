720fea33de4b48cbdb0715e8461635946c6e7b3c
Jim <zhangpengyi@taijihuabao.com>
2021-09-13 16:30:17 +0800

    打印日志

--- routes/index.js
+++ routes/index.js
@@ -9,34 +9,37 @@ var cors = require('cors')
 // 参考：https://luneshao.github.io/2020/2020-04-07-fluent-ffmpeg-api/
 // 参考：https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/blob/master/examples/
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Taiji Video' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Taiji Video'});
 });
 
-router.post('/video/process/audio',function (req,res) {
-  logger.info(req.body);
+router.post('/video/process/audio', function (req, res) {
+    logger.info(req.body);
 
-  res.status(201).send()  // 设置请求成功状态码 201
+    res.status(201).send()  // 设置请求成功状态码 201
 });
 
 router.get('/test/video-to-audio', (req, res) => {
-  res.contentType('audio/mp3');
-  res.attachment('myfile.mp3');
-  // 测试视频：http://192.168.5.39:8099/storage/lizhi-GuanYuZhengZhouDeJiYi.mp4
-  var pathToAudio = 'http://192.168.5.39:8099/storage/lizhi-GuanYuZhengZhouDeJiYi.mp4';
-  ffmpeg(pathToAudio)
-      .toFormat('mp3')
-      .on('end', function(err) {
-        console.log('done!')
-      })
-      .on('error', function(err) {
-        console.log('an error happened: ' + err.message);
-      })
-      .pipe(res, {end: true})
+    res.contentType('audio/mp3');
+    res.attachment('myfile.mp3');
+    // 测试视频：http://192.168.5.39:8099/storage/lizhi-GuanYuZhengZhouDeJiYi.mp4
+    var pathToAudio = 'http://192.168.5.39:8099/storage/lizhi-GuanYuZhengZhouDeJiYi.mp4';
+    ffmpeg(pathToAudio)
+        .toFormat('mp3')
+        .on('end', function (err) {
+            logger.info('done!')
+            console.log('done!')
+        })
+        .on('error', function (err) {
+            console.log('an error happened: ' + err.message);
+            logger.info('an error happened: ' + err.message);
+        })
+        .pipe(res, {end: true})
 });
 
 router.get('/test/video-to-images', (req, res) => {
     var pathToAudio = 'http://192.168.5.39:8099/storage/lizhi-GuanYuZhengZhouDeJiYi.mp4';
+
     // offsets 以毫秒为单位
     async function a() {
         extractFrames({
@@ -54,15 +57,16 @@ router.get('/test/video-to-images', (req, res) => {
                 80000
             ]
         }).then(r => function () {
-            console.log("finish")
+            logger.info("finish")
         })
     }
+
     a().then(r => function () {
-        console.log('finished')
+        logger.info('finished')
     });
     res.status(200).send({
-        'message' : 'success',
-        'status' : 200,
+        'message': 'success',
+        'status': 200,
     });
 });
 
